f0d8ed137a940a5ed29134d702dc5bc6ea59e514
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-03 21:42:47 -0400

    coverted nested array to JS object

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function arrayToObj() {
-   // your code goes here  
+function arrayToObj(aList) {
+   let arr = {};
+   aList.forEach((element) => {
+      arr[element[0]] = element[1];
+   })
+   return arr
 }
 
 module.exports = arrayToObj;
